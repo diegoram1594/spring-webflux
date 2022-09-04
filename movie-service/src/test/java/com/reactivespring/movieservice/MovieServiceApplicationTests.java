96883532ab6b82b9bc0104966e6b9291826607a3
Diego Ramirez <diegoramirez1594@gmail.com>
2022-09-04 13:07:44 -0500

    Module movieService

---
+++ movie-service/src/test/java/com/reactivespring/movieservice/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivespring.movieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
